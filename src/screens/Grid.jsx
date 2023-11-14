fee0faa4995f7d4dc2c095a51e141585eccf904f
mf0x <mikefox9000@gmail.com>
2023-11-14 15:59:08 -0500

    create Grid and Book screens

---
+++ src/screens/Grid.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { getAll } from "../services/actions";
+// import Thumbnail from "../components/Thumbnail";
+
+function Grid() {
+  const [thumbnails, setThumbnails] = useState([]);
+
+  useEffect(() => {
+    fetchThumbnails();
+  }, []);
+
+  async function fetchThumbnails() {
+    const allThumbnails = await getAll();
+    setThumbnails(allThumbnails);
+  }
+
+  return (
+    <div>
+      <h1>All the books!</h1>
+      <div className="grid">
+        {thumbnails.map((thumbnail) => (
+          <Thumbnail thumbnail={thumbnail} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Grid;
