b20a5864e29d544f63a69fcc1bf71c48d19b0060
elisacris98 <cristina.elisa98@gmail.com>
2023-11-16 11:29:42 -0500

    edited home and css

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
     return (
       <div>
-        
-        <img className="picture" src="https://i0.wp.com/cms.babbel.news/wp-content/uploads/2022/01/Most_Beautiful_Libraries.png" alt="libary of books" />
+        <div class= "test">
+            <h1>Welcome</h1>
+        </div>
+        {/* <img className="picture" src="https://i0.wp.com/cms.babbel.news/wp-content/uploads/2022/01/Most_Beautiful_Libraries.png" alt="libary of books" /> */}
       </div>
     )
   }
