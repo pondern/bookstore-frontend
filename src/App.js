f1710b20ab466a7ab53fe515b08da1856f613c22
elisacris98 <cristina.elisa98@gmail.com>
2023-11-16 09:45:03 -0500

    added picture and header to home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavBar from './components/Nav.jsx'; 
 import "./App.css"
+import Home from './screens/Home.jsx';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      < Home />
       {/* Other components and content */}
     </div>
   );
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
       <div>
-        <h1 className="home">Welcome!</h1>
-        <img src="https://i0.wp.com/cms.babbel.news/wp-content/uploads/2022/01/Most_Beautiful_Libraries.png" alt="libary of books" />
+        <h1 className="home">Welcome</h1>
+        <img className="picture" src="https://i0.wp.com/cms.babbel.news/wp-content/uploads/2022/01/Most_Beautiful_Libraries.png" alt="libary of books" />
       </div>
     )
   }
