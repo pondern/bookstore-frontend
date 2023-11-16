b867d137164fca69a8f19c21451e117d41a82e90
elisacris98 <cristina.elisa98@gmail.com>
2023-11-16 10:02:38 -0500

    still working on home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div>
-        <h1 className="home">Welcome</h1>
+        
         <img className="picture" src="https://i0.wp.com/cms.babbel.news/wp-content/uploads/2022/01/Most_Beautiful_Libraries.png" alt="libary of books" />
       </div>
     )
