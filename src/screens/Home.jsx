0fa4b09fa32458e723fd2ca01974e430bb1dd531
elisacris98 <cristina.elisa98@gmail.com>
2023-11-16 16:12:36 -0500

    fetched info for homepage

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,35 @@
-export default function Home() {
+import React from 'react'
+import {useEffect,useState} from 'react';
+import {getBooks} from '../services/books.js';
+import Book from '../components/Book.jsx';
+
+
+function Home() {
+
+  const [books , setBooks] = useState([])
+
+  useEffect(()=>{
+    fetchBooks()
+  },[]) //only fire this function one time 
+
+  async function fetchBooks () {
+    const allBooks = await getBooks()
+    setBooks(allBooks)
+  }
+
     return (
       <div>
         <div class= "test">
             <h1>Welcome</h1>
         </div>
-        <div className="horizontal-scroll-container">
+        <div className="horizontal-scroll-container"> 
+        {
+          books.map((book)=>(
+          <Book book={book}/>
+          )
+          )
+
+        }
         
 
         </div>
@@ -16,3 +41,11 @@ export default function Home() {
 
 
   }
+
+export default Home
+
+
+
+
+
+   
