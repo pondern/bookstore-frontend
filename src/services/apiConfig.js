4685dcf1d1712af8ba241f1c9fe98edcd8ecc2ca
mf0x <mikefox9000@gmail.com>
2023-11-17 11:04:51 -0500

    fix apiConfig.js environments

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -9,8 +9,8 @@ const getToken = () => {
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://ga-bookstore-8724ed2991a2.herokuapp.com/apiapi"
-      : "https://ga-bookstore-8724ed2991a2.herokuapp.com/api",
+      ? "https://ga-bookstore-8724ed2991a2.herokuapp.com/api"
+      : "http://localhost:3000/api",
 });
 
 api.interceptors.request.use(
