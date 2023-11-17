ed8e3e5092e8e52f4cc036037b29146b5f810346
elisacris98 <cristina.elisa98@gmail.com>
2023-11-16 23:37:05 -0500

    added scroll action

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,51 +1,35 @@
-import React from 'react'
-import {useEffect,useState} from 'react';
-import {getBooks} from '../services/books.js';
+import React, { useEffect, useState } from 'react';
+import { getBooks } from '../services/books.js';
 import Book from '../components/Book.jsx';
 
-
 function Home() {
+  const [books, setBooks] = useState([]);
 
-  const [books , setBooks] = useState([])
+  useEffect(() => {
+    fetchBooks();
+  }, []); // Only fire this function one time
 
-  useEffect(()=>{
-    fetchBooks()
-  },[]) //only fire this function one time 
-
-  async function fetchBooks () {
-    const allBooks = await getBooks()
-    setBooks(allBooks)
+  async function fetchBooks() {
+    const allBooks = await getBooks();
+    setBooks(allBooks);
   }
 
-    return (
-      <div>
-        <div class= "test">
-            <h1>Welcome</h1>
-        </div>
-        <div className="horizontal-scroll-container"> 
-        {
-          books.map((book)=>(
-          <Book book={book}/>
-          )
-          )
-
-        }
-        
-
+  return (
+    <div>
+      <div className="test">
+        <h1>Welcome</h1>
+      </div>
+      <div className="scrollable-books-container">
+        <div className="horizontal-scroll-container">
+          {books.slice(0, 10).map((book) => (
+            <div key={book.id} className="book-item">
+              <Book book={book} />
+            </div>
+          ))}
         </div>
       </div>
-    
+    </div>
+  );
+}
 
-    
-    )
-
-
-  }
-
-export default Home
-
-
-
-
-
-   
+export default Home;
