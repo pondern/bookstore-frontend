292515d7a6b80b3bfcce6030aabdd7fedab21373
mf0x <mikefox9000@gmail.com>
2023-11-18 19:22:07 -0500

    comment submit works, but doesn't render until page refresh

--- src/components/SavedBook.jsx
+++ src/components/SavedBook.jsx
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
 import ReactStars from "react-rating-stars-component";
 import { editBookReview } from "../services/libraries";
 
 function SavedBook({ savedBook, libId }) {
+  const [review, setReview] = useState(savedBook.comment);
+
   const ratingChanged = (newRating) => {
     editBookReview(libId, savedBook._id, { stars: newRating });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can handle the review submission, like sending it to an API or processing it further
+    editBookReview(libId, savedBook._id, { comment: review });
+    console.log("Submitted Review:", review);
+    // Optionally, you can reset the form after submission
+    setReview("");
+  };
+
+  const handleChange = (e) => {
+    setReview(e.target.value);
   };
 
   return (
@@ -21,6 +37,20 @@ function SavedBook({ savedBook, libId }) {
           size={24}
           activeColor="#ffd700"
         />
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Leave a review:
+            <input
+              type="text"
+              name="review"
+              value={review}
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <p>{savedBook.comment}</p>
       </div>
     </div>
